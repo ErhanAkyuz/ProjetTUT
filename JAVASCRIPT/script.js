6eeee27afa19cf552aae93a6b0d7656f8bf40803
EIdras <simon72610@gmail.com>
2020-11-29 14:34:51 +0100

    V0.0.1.7.6

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -4,7 +4,10 @@ window.onload = () => {
 
     // Ecouteur d'évènement sur scroll
     window.addEventListener("scroll", () =>{
-
+        var nav = document.getElementsByClassName("menu"); // Récupération de l'élément 'menu' dans la variable 'nav'
+        var navPosY = nav.offsetTop;                       // Récupération de la position horizontale de 'nav' dans la variable 'navPosY'
+        console.log(navPosY);                              // Affichage dans la console de la valeur de la variable 'navPosY'
+        
         // Calcul de la hauteur "utile" du document
         let hauteur = document.documentElement.scrollHeight - window.innerHeight
 
@@ -15,7 +18,7 @@ window.onload = () => {
         let largeur = document.documentElement.clientWidth
         var el = document.getElementsByClassName('body')
         el = el.clientHeight
-        console.log(el)
+        //console.log(el)
 
         // Calcul de la largeur de la barre
         let barre = position / hauteur * largeur
@@ -44,20 +47,13 @@ function toggle_text() {
     }
   }
 */
-window.addEventListener('load', horloge);
-
-function horloge() {
-	let d = new Date();
-	document.getElementById('heure').innerHTML = d.toLocaleTimeString();
-	setTimeout(horloge, 1000);
-}
 
 document.addEventListener('DOMContentLoaded', function () {
 	let cache = document.getElementById('bouton');
-	cache.addEventListener('click', cacheHorloge);
+	cache.addEventListener('click', cacheTexte);
 	document.getElementById('tog1').style.display = 'none';
 
-	function cacheHorloge() {
+	function cacheTexte() {
 		let para = document.getElementById('tog1');
 		if (para.style.display == 'none') {
       para.style.display = 'block';
@@ -72,10 +68,10 @@ document.addEventListener('DOMContentLoaded', function () {
 /* TEST D'UN AUTRE BOUTON*/                     /* POUR CREER UN NOUVEAU BOUTON IL SUFFIT DE CHANGER LES ID DANS LE HTML ET LE JS*/
 document.addEventListener('DOMContentLoaded', function () {
 	let cache = document.getElementById('bouton2'/*ICI*/);
-	cache.addEventListener('click', cacheHorloge);
+	cache.addEventListener('click', cacheTexte);
 	document.getElementById('tog2'/*ICI*/).style.display = 'none';
 
-	function cacheHorloge() {
+	function cacheTexte() {
 		let para = document.getElementById('tog2'/*ET ICI*/);
 		if (para.style.display == 'none') {
       para.style.display = 'block';
