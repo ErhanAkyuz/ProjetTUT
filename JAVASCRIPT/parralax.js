1a50ca2259eb23e5637828f32215484c0bf5900d
EIdras <simon72610@gmail.com>
2020-12-01 11:14:05 +0100

    V0.0.1.7.7

---
+++ JAVASCRIPT/parralax.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">	
+	$(document).ready(function()
+    {		
+	    $('body').parallax("center", 0, 0.1, true); 
+    }
+    )
+</script>
--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -4,10 +4,11 @@ window.onload = () => {
 
     // Ecouteur d'évènement sur scroll
     window.addEventListener("scroll", () =>{
+
         var nav = document.getElementsByClassName("menu"); // Récupération de l'élément 'menu' dans la variable 'nav'
         var navPosY = nav.offsetTop;                       // Récupération de la position horizontale de 'nav' dans la variable 'navPosY'
         console.log(navPosY);                              // Affichage dans la console de la valeur de la variable 'navPosY'
-        
+
         // Calcul de la hauteur "utile" du document
         let hauteur = document.documentElement.scrollHeight - window.innerHeight
 
@@ -32,27 +33,12 @@ window.onload = () => {
 // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ //
 
 // ---------- Bouton qui affiche le texte ---------- //
-/*
-function toggle_text() {
-    var span = document.getElementById("span_txt");
-    if(span.innerHTML != "") {
-      span.innerHTML = "";
-    } else {
-      //span.innerHTML = "Test (JS)";
-        span.innerHTML = "<p class='article'> <strong>Le handicap psychique</strong> est une catégorie de handicap souvent confondue avec le handicap mental.Ce handicap touche plus de 80% des handicapés français et tocuhera 1/3 de la population dans sa vie. Ses manifestations sont nombreuses, allant de <strong>phobies</strong>,<strong>traumatismes</strong>,<strong>troubles mentaux</strong> et <strong>encore depressions.</strong></br><strong>L'handicap psychique</strong> est la conséquence de maladies mentales (à ne pas confondre avec le handicap mental) entrainant un déficit relationnel, desdifficultés de concentration, une grande variabilité dans la possibilité d'utilisation des capacités alors que la personne garde des facultés intellectuelles dites normales.La reconnaissance du handicap psychique s’effectue souvent par l’observation des conséquences de la maladie mentale dans le vécu de la personne. Celles-ci peuvent donc être extrêmement variées en fonction des individus et de leur pathologie. De fait, il n’existe pas de définition arrêtée du handicap psychique.</br>Voici quelque exemple : <ul><li>les troubles graves de la personnalité (personnalité borderline, par exemple)</li><li>certains troubles névrotiques graves comme les TOC (troubles obsessionnels compulsifs);</li><li>les Troubles du Spectre Autistique (TSA) ou troubles du développement (TDE);</li><li>les psychoses&nbsp;: altération de la perception du rapport à la réalité. Exemple&nbsp;: schizophrénie;</li><li>les troubles de l’humeur&nbsp;: maladies de l’affect. Exemple&nbsp;: dépression ou troubles bipolaires;</li></ul></p>";
-      // Ca fonctionne mais le code est pas beau
-        document.getElementById("span_button").style.position = "absolute";
-        document.getElementById("span_button").style.top = "800px";
-        document.getElementById("span_button").style.left = "1050px";
-    }
-  }
-*/
 
 document.addEventListener('DOMContentLoaded', function () {
 	let cache = document.getElementById('bouton');
 	cache.addEventListener('click', cacheTexte);
-	document.getElementById('tog1').style.display = 'none';
-
+   document.getElementById('tog1').style.display = 'none';
+   
 	function cacheTexte() {
 		let para = document.getElementById('tog1');
 		if (para.style.display == 'none') {
@@ -107,5 +93,5 @@ document.addEventListener('DOMContentLoaded', function () {
 // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ //
 
 // -------- Savoir si un élément est sticky -------- //
-//#==================___EN COURS___==================// 2%
-//to check when element get's position sticky
+//###================___EN COURS___==================// 4%
+// To check when element get's position sticky
