73e7ce63708bc132c84d7e1b2f88436a5e410d9b
Grizfreak <ninotnaaubert@gmail.com>
2020-12-04 17:22:06 +0100

    PARALLAX FONCTIONNEL
    j'adore

---
+++ JAVASCRIPT/parrallax_script.js
@@ -0,0 +1,5 @@
+	$(document).ready(function(){		
+	    $('#slide1').parallax("center", 500, 0.1, true);
+		$('#slide2').parallax("center", 1280, 0.1, true);
+		$('#bodyslide').parallax("center", 1280, 0.4, true);
+	})
