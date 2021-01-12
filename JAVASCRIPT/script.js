bbab7ae5e7ec4f3e49ec459bc7839b93903d1249
EIdras <simon72610@gmail.com>
2021-01-12 14:25:20 +0100

    V0.0.1.9.6.7
    Supression de portions de code inutiles dans le Javascript, et commentaires

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,13 +1,9 @@
-// ---------- Barre de progression du site ---------- //
+// ------------- Barre de progression du site ------------- //
 window.onload = () => {
     document.getElementById("progress").style.width = 0+"px"
 
     // Ecouteur d'évènement sur scroll
     window.addEventListener("scroll", () =>{
-
-        var nav = document.getElementsByClassName("menu"); // Récupération de l'élément 'menu' dans la variable 'nav'
-        var navPosY = nav.offsetTop;                       // Récupération de la position horizontale de 'nav' dans la variable 'navPosY'
-        console.log(navPosY);                              // Affichage dans la console de la valeur de la variable 'navPosY'
 
         // Calcul de la hauteur "utile" du document
         let hauteur = document.documentElement.scrollHeight - window.innerHeight
@@ -17,9 +13,6 @@ window.onload = () => {
 
         // Récupération de la largeur de la fenêtre
         let largeur = document.documentElement.clientWidth
-        var el = document.getElementsByClassName('body')
-        el = el.clientHeight
-        //console.log(el)
 
         // Calcul de la largeur de la barre
         let barre = position / hauteur * largeur
@@ -28,18 +21,25 @@ window.onload = () => {
         document.getElementById("progress").style.width = barre+"px"
     })
 }
-// ----------------------- Fin -----------------------  //
+// -------------------------- Fin --------------------------  //
 
 
 // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ //
 
 
-// ---------- Boutons qui affichent le texte ---------- //
-function toggleDisplay(id, buttonid, text){
-    const element = document.getElementById(id);
-    const button = document.getElementById(buttonid);
-    if(!element || !button) return;
-    element.classList.toString().includes("hidden") ? button.innerHTML = text ? text.display : "Voir moins" : button.innerHTML = text ? text.hide :"Voir plus";
-    element.classList.toString().includes("hidden") ? element.classList.remove("hidden") : element.classList.add("hidden");
+// ------------- Boutons qui affichent le texte ------------- //
+function toggleDisplay(id, buttonid, text){                     // La fonction toggleDisplay est lancée à chaque clic sur un bouton
+    const element = document.getElementById(id);                // On récupère la section cachée par l'id renseigné
+    const button = document.getElementById(buttonid);           // On récupère le bouton à l'aide de son id
+
+    if(!element || !button) return;                             // On rentre dans la fonction seulement si les 'element' et 'button' existent
+    element.classList.toString().includes("hidden") ?           // Test si la section possède la classe "hidden"
+    button.innerHTML = text ?                                   // Test si le bouton comporte du texte quand il est cliqué
+     text.display : "Voir moins" :                              // Si non on change le texte du bouton pour 'Voir moins'
+     button.innerHTML = text ?                                  // Test si le bouton comporte du texte 
+     text.hide : "Voir plus";                                   // Si non on change le texte du bouton pour 'Voir plus'
+
+    element.classList.toString().includes("hidden") ?                       // Test si la section possède la classe "hidden"
+    element.classList.remove("hidden") : element.classList.add("hidden");   // Si c'est le cas, on la supprime, sinon on la rajoute
 }
-// ----------------------- Fin -----------------------  //
+// -------------------------- Fin --------------------------  //
